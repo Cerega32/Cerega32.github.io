ad9eccee1e0908a09f28e1a19bfa0b1f922966df
Cerega32 <Cerega32@mail.ru>
2018-09-25 21:14:45 +0300

    v2.1

--- js/common.js
+++ js/common.js
@@ -3,28 +3,32 @@ $(function(){
 	scroll();
 	
 	//scroll to top
-	$('.scroll-top').click(function(){
-		$('html, body').animate({ scrollTop: 0 }, 500);
+	$(".scroll-top").click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 500);
 		return false;
 	}); 
  
 	//show up-button
 	$(document).scroll(function(){
-		var y = $(this).scrollTop();
-		if (y > 770) {
-			$('.scroll-top').removeClass('scroll-top--hidden');
-		} else {
-			$('.scroll-top').addClass('scroll-top--hidden');
-		}
+		scroll();
 	});
- 
+	
+	$(".navbar__btn").click(function(){
+		$(".navbar__search").removeClass("navbar__search--active");
+		$(".navbar__search").focus();
+	});
+	
+	$('.navbar__search').focusout(function(){
+		$(".navbar__search").addClass("navbar__search--active");
+	});
+	
 });
 
 function scroll() {
 	var y = $(this).scrollTop();
   if (y > 770) {
-			$('.scroll-top').removeClass('scroll-top--hidden');
+			$(".scroll-top").removeClass("scroll-top--hidden");
 		} else {
-			$('.scroll-top').addClass('scroll-top--hidden');
+			$(".scroll-top").addClass("scroll-top--hidden");
 		}
 }
