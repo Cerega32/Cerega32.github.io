8a99576f4eb8cc5f8d66e3653ed17b37ee899f08
Cerega32 <Cerega32@mail.ru>
2018-09-27 19:15:14 +0300

    v3

--- js/common.js
+++ js/common.js
@@ -1,3 +1,47 @@
+(function(){
+var a = document.querySelector('#sidebar'), b = null, P = 0;
+window.addEventListener('scroll', Ascroll, false);
+document.body.addEventListener('scroll', Ascroll, false);
+function Ascroll() {
+  if (b == null) {
+    var Sa = getComputedStyle(a, ''), s = '';
+    for (var i = 0; i < Sa.length; i++) {
+      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+      }
+    }
+    b = document.createElement('div');
+    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+    a.insertBefore(b, a.firstChild);
+    var l = a.childNodes.length;
+    for (var i = 1; i < l; i++) {
+      b.appendChild(a.childNodes[1]);
+    }
+    a.style.height = b.getBoundingClientRect().height + 'px';
+    a.style.padding = '0';
+    a.style.border = '0';
+  }
+  var Ra = a.getBoundingClientRect(),
+      R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('#articles').getBoundingClientRect().bottom);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
+  if ((Ra.top - P) <= 0) {
+    if ((Ra.top - P) <= R) {
+      b.className = 'stop';
+      b.style.top = - R +'px';
+    } else {
+      b.className = 'sticky';
+      b.style.top = P + 'px';
+    }
+  } else {
+    b.className = '';
+    b.style.top = '';
+  }
+  window.addEventListener('resize', function() {
+    a.children[0].style.width = getComputedStyle(a, '').width
+  }, false);
+}
+})()
+
+
 $(function(){
 
 	scroll();
@@ -26,9 +70,20 @@ $(function(){
 
 function scroll() {
 	var y = $(this).scrollTop();
-  if (y > 770) {
+  if (y > getCoords(blog[0]).top) {
 			$(".scroll-top").removeClass("scroll-top--hidden");
+			$(".navbar").addClass("navbar--fixed");
 		} else {
 			$(".scroll-top").addClass("scroll-top--hidden");
+			$(".navbar").removeClass("navbar--fixed");
 		}
 }
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
+  return {
+    top: box.top + pageYOffset - 50
+  };
+
+}
+var blog = document.getElementsByClassName("blog");
+
