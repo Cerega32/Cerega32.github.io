60630dbfa6926672606ddc61f06a69e730885e7f
Cerega32 <Cerega32@mail.ru>
2019-05-01 11:28:45 +0300

    01.05

---
+++ visit/server/server_v2/js/common.js
@@ -0,0 +1,438 @@
+var markersData = [
+	{
+		lat: 62.541147,     // Широта
+		lng: 113.980685,    // Долгота
+		name: "1комнатная кв - 3500р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Ленина 23"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 62.540104,
+		lng: 113.988145,
+		name: "1комнатная кв - 3000р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Ленина 43"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 62.543026,
+		lng: 113.977408,
+		name: "1комнатная кв - 3500р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Советская 3"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 62.541832,
+		lng: 113.983377,
+		name: "2комнатная кв - 4500р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Советская 13к5"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 62.546399,
+		lng: 113.983133,
+		name: "1комнатная кв - 3500р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Тихонова 15/2"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 62.543129,
+		lng: 113.990280,
+		name: "1комнатная кв - 3000р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Тихонова 16а"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 62.543820,
+		lng: 113.977911,
+		name: "2комнатная кв - 4500р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Павлова 8"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 62.541982,
+		lng: 113.973978,
+		name: "1комнатная кв - 3500р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Ойунского 36"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 62.543572,
+		lng: 113.971735,
+		name: "2комнатная кв - 4500р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. 50 лет Октября 1"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 66.412468,
+		lng: 112.246203,
+		name: "1комнатная кв - 4000р/сутки", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Новый город микрорайон 19"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 66.413671,
+		lng: 112.253277,
+		name: "-", // Произвольное название, которое будем выводить в информационном окне
+		address: "ул. Новый город микрорайон 3"   // Адрес, который также будем выводить в информационном окне
+	},
+	{
+		lat: 66.413892,
+		lng: 112.242271,
+		name: "1комнатная кв - 3000р/сутки",
+		address: "ул. Новый город микрорайон 33"
+	},
+	{
+		lat: 62.542157,
+		lng: 113.981590,
+		name: "2комнатная кв - 5000р/сутки",
+		address: "ул. Cоветская 11/4"
+	},
+	{
+		lat: 66.415471,
+		lng: 112.243495,
+		name: "1комнатная кв - 5000р/сутки",
+		address: "ул. Новый город микрорайон 13"
+	}
+
+];
+var map, infoWindow;
+function initMap() {
+    var centerLatLng = new google.maps.LatLng(65.209010, 112.686254);
+    var mapOptions = {
+        center: centerLatLng,
+        zoom: 5,
+				gestureHandling: 'greedy'
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    // Создаем объект информационного окна и помещаем его в переменную infoWindow
+    // Так как у каждого информационного окна свое содержимое, то создаем пустой объект, без передачи ему параметра content
+    infoWindow = new google.maps.InfoWindow();
+    // Отслеживаем клик в любом месте карты
+    google.maps.event.addListener(map, "click", function() {
+        // infoWindow.close - закрываем информационное окно.
+        infoWindow.close();
+    });
+    // Перебираем в цикле все координата хранящиеся в markersData
+    for (var i = 0; i < markersData.length; i++){
+        var latLng = new google.maps.LatLng(markersData[i].lat, markersData[i].lng);
+        var name = markersData[i].name;
+        var address = markersData[i].address;
+        // Добавляем маркер с информационным окном
+        addMarker(latLng, name, address);
+    }
+}
+// Функция добавления маркера с информационным окном
+function addMarker(latLng, name, address) {
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        title: name
+    });
+    // Отслеживаем клик по нашему маркеру
+    google.maps.event.addListener(marker, "click", function() {
+        // contentString - это переменная в которой хранится содержимое информационного окна.
+        var contentString = '<div class="infowindow">' +
+                                '<h3>' + name + '</h3>' +
+                                '<p>' + address + '</p>' +
+                            '</div>';
+        // Меняем содержимое информационного окна
+        infoWindow.setContent(contentString);
+        // Показываем информационное окно
+        infoWindow.open(map, marker);
+    });
+}
+
+
+$(".appartments__more").click(function() {
+	$(".appartments--hide")
+		.css("display" , "block")
+		.animate({opacity: 1, top: '0'}, 500);
+	$(".appartments__more").css("display" , "none");
+	$(".appartments__less").css("display" , "inline");
+});
+
+$(".appartments__less").click(function() {
+	setTimeout(function() {$(".appartments--hide").css("display" , "none")}, 1000);
+	$(".appartments--hide").animate({opacity: 0, top: '-100px'}, 1000);
+	$(".appartments__more").css("display" , "inline");
+	$(".appartments__less").css("display", "none");
+	$('html, body').animate({
+		scrollTop: $(".appartments").offset().top - 100
+	}, 1000);
+});
+
+$(".appartments__more1").click(function() {
+	$(".appartments--hide1")
+		.css("display" , "block")
+		.animate({opacity: 1, top: '0'}, 500);
+	$(".appartments__more1").css("display" , "none");
+	$(".appartments__less1").css("display" , "inline");
+});
+
+$(".appartments__less1").click(function() {
+	setTimeout(function() {$(".appartments--hide1").css("display" , "none")}, 1000);
+	$(".appartments--hide1").animate({opacity: 0, top: '-100px'}, 1000);
+	$(".appartments__more1").css("display" , "inline");
+	$(".appartments__less1").css("display", "none");
+	$('html, body').animate({
+		scrollTop: $(".appartments__title1").offset().top - 100
+	}, 1000);
+});
+
+$('[data-fancybox="flat1"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/kvart-otel-quot-vip-komfort-quot.ru.html">booking.com</a> и <a href="https://www.booking.com/hotel/ru/kvart-otel-quot-vip-komfort-quot.ru.html">суточну.ру</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat2"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://mirnii.sutochno.ru/400801">суточну.ру</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat3"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/vip-komfort-sovetskaia-3.ru.html">booking.com</a> и <a href="https://mirnii.sutochno.ru/363449">суточну.ру</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat4"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/sovetskaia-ulitsa-13-korp-5.ru.html">booking.com</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat5"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/vip-komfort-tikhonova-15-47-2.ru.html">booking.com</a> и <a href="https://mirnii.sutochno.ru/496195">суточну.ру</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat6"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://mirnii.sutochno.ru/471853">суточну.ру</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat7"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/apartamenty-na-pavlova-8.ru.html">booking.com</a> и <a href="https://mirnii.sutochno.ru/458073">суточну.ру</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat8"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/vip-kosfort-oiunskogo-36.ru.html">booking.com</a> и <a href="https://mirnii.sutochno.ru/431813">суточну.ру</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat9"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/dvushka-na-50-let-oktiabria.ru.html">booking.com</a> и <a href="https://mirnii.sutochno.ru/543781">суточну.ру</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat10"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/vip-komfort-udachnyi-novyi-gorod-19.ru.html">booking.com</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat12"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/udachnyi-n-gorod-33.ru.html">booking.com</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat13"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/apartamenty-na-sovetskoi-11-47-4.ru.html">booking.com</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$('[data-fancybox="flat14"]').fancybox({
+	caption : 'Эти апартаменты на <a href="https://www.booking.com/hotel/ru/vip-komfort-udachnyi-novyi-gorod-13.ru.html">booking.com</a>',
+	infobar: true,
+  thumbs : {
+    autoStart : false
+  }
+});
+
+$(document).ready(function () { 
+	var show = true;
+	var countbox = ".hotel__numbers";
+	$(window).on("scroll load resize", function () {
+		if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+		var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+		var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+		var w_height = $(window).height(); // Высота окна браузера
+		var d_height = $(document).height(); // Высота всего документа
+		var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+		if (document.documentElement.clientWidth > 768) {
+			if (w_top + 800 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+				$('.hotel__count').css('opacity', '1');
+				$('.spincrement').spincrement({
+						thousandSeparator: "",
+						duration: 1200
+				});
+				$(".spincrement1").spincrement({
+					duration: 1200
+				});
+				show = false;
+			}
+		} else {
+			if (w_top + 400 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+				$('.hotel__count').css('opacity', '1');
+				$('.spincrement').spincrement({
+						thousandSeparator: "",
+						duration: 1200
+				});
+				$(".spincrement1").spincrement({
+					duration: 1200
+				});
+				show = false;
+			}
+		}
+	});
+});
+
+scroll();
+
+$(document).scroll(function(){
+	scroll();
+});
+
+
+function scroll() {
+	if (document.documentElement.clientWidth > 1200) {
+		var y = $(this).scrollTop();
+		if (y > 100) {
+			$(".navbar__logo").addClass("navbar__logo--inactive");
+			$(".navbar__logo--active").removeClass("navbar__logo--inactive");
+			$(".navbar").addClass("navbar--active");
+		} else {
+			$(".navbar__logo").removeClass("navbar__logo--inactive");
+			$(".navbar__logo--active").addClass("navbar__logo--inactive");
+			$(".navbar").removeClass("navbar--active");
+		}
+	}
+}
+
+$('.header__btn, .contacts__btn').click( function(event){ // лoвим клик пo ссылки с id="go"
+	event.preventDefault(); // выключaем стaндaртную рoль элементa
+	$('.overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+		function(){ // пoсле выпoлнения предъидущей aнимaции
+			if (document.documentElement.clientWidth > 768) {
+			$('.header__form') 
+				.css('display', 'flex') // убирaем у мoдaльнoгo oкнa display: none;
+				.animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+	} else {
+		$('.header__form') 
+				.css('display', 'flex') // убирaем у мoдaльнoгo oкнa display: none;
+				.animate({opacity: 1}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+	}
+	});
+});
+
+$(document).keyup(function(e) {
+	 if (e.keyCode == 27) { 
+		if (document.documentElement.clientWidth > 768) {
+		$('.header__form')
+			.animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+				function(){ // пoсле aнимaции
+					$(this).css('display', 'none'); // делaем ему display: none;
+					$('.overlay').fadeOut(400); // скрывaем пoдлoжку
+				}
+			);
+	} else {
+		$('.header__form')
+			.animate({opacity: 0}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+				function(){ // пoсле aнимaции
+					$(this).css('display', 'none'); // делaем ему display: none;
+					$('.overlay').fadeOut(400); // скрывaем пoдлoжку
+				}
+			);
+		}
+	}
+});
+
+$('.overlay, .header__form-close').click( function(){ // лoвим клик пo крестику или пoдлoжке
+	if (document.documentElement.clientWidth > 768) {
+		$('.header__form')
+			.animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+				function(){ // пoсле aнимaции
+					$(this).css('display', 'none'); // делaем ему display: none;
+					$('.overlay').fadeOut(400); // скрывaем пoдлoжку
+				}
+			);
+	} else {
+		$('.header__form')
+			.animate({opacity: 0}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+				function(){ // пoсле aнимaции
+					$(this).css('display', 'none'); // делaем ему display: none;
+					$('.overlay').fadeOut(400); // скрывaем пoдлoжку
+				}
+			);
+		}
+	});
+
+$('.navbar__btn').click(function() {
+	$('.navbar__right--active').toggleClass("navbar__right--open");
+	$('.navbar__right--active').addClass("navbar__right--opacity");
+})
+
+function closeMenu() {
+	$('.navbar__right--active').toggleClass("navbar__right--open");
+}
+$(document).ready(function(){
+	$(".navbar__nav").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top - 110}, 1500);
+	});
+});
+$('.header__menu-close').click(function() {
+	$('.navbar__right--active').toggleClass("navbar__right--open");
+})
+
+$("#form").submit(function() {
+	$.ajax({
+		type: "POST",
+		url: "mail.php",
+		data: $(this).serialize()
+	}).done(function() {
+		$(this).find("input").val("");
+		alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+		$("#form-2").trigger("reset");
+	});
+	return false;
+});
+
+$("#form-2").submit(function() {
+	$.ajax({
+		type: "POST",
+		url: "mail.php",
+		data: $(this).serialize()
+	}).done(function() {
+		$(this).find("input").val("");
+		alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+		$("#form-1").trigger("reset");
+	});
+	return false;
+});
