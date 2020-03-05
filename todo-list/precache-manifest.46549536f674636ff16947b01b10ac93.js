1faab72ae1425806bc1af13523c4618cf54b31ea
SergeyG <Cerega32@mail.ru>
2020-03-05 09:01:24 +0300

    Added todo-list

---
+++ todo-list/precache-manifest.46549536f674636ff16947b01b10ac93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fd775440cdff546f102",
+    "url": "/static/css/main.2df3012f.chunk.css"
+  },
+  {
+    "revision": "5fd775440cdff546f102",
+    "url": "/static/js/main.5fd77544.chunk.js"
+  },
+  {
+    "revision": "bc951937f3a44b7747ac",
+    "url": "/static/js/1.bc951937.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "002f7dcb8c80417e6219ccd50ad78800",
+    "url": "/index.html"
+  }
+];
