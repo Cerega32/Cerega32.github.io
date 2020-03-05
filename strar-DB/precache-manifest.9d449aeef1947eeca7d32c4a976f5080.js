b5aa0b3c0275762a8419330aca6aedf5e9021660
SergeyG <Cerega32@mail.ru>
2020-03-05 08:20:30 +0300

    Added star-DB react

---
+++ strar-DB/precache-manifest.9d449aeef1947eeca7d32c4a976f5080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188f705e901a78e461c8fd8926974c3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc2d2113b5ced03a5260",
+    "url": "/static/css/main.170698ff.chunk.css"
+  },
+  {
+    "revision": "7a5d95d95c50d786ca76",
+    "url": "/static/js/2.f77f932a.chunk.js"
+  },
+  {
+    "revision": "cc2d2113b5ced03a5260",
+    "url": "/static/js/main.6f501b6f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
