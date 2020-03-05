93dcc41cc77651194c0a1caced76f5315f270c9d
SergeyG <Cerega32@mail.ru>
2020-03-05 09:44:39 +0300

    Added SPA - React

---
+++ board-games/precache-manifest.641f2ea4f972a4e7fc9907e67254fb48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e21dac6ca4a504d3d718beb78664a6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eedafe9588505f89add1",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "c611d6a4533e1e4be488",
+    "url": "/static/css/main.a3e1d216.chunk.css"
+  },
+  {
+    "revision": "eedafe9588505f89add1",
+    "url": "/static/js/2.fbb55a0f.chunk.js"
+  },
+  {
+    "revision": "4d05604127059121c8545b8fa9a7e3f9",
+    "url": "/static/js/2.fbb55a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c611d6a4533e1e4be488",
+    "url": "/static/js/main.9212a613.chunk.js"
+  },
+  {
+    "revision": "98cabcafc6c739163e0c",
+    "url": "/static/js/runtime-main.b4c7db15.js"
+  },
+  {
+    "revision": "02f448f0f6df3781f073928a3e54cdf6",
+    "url": "/static/media/logo.02f448f0.png"
+  }
+]);
